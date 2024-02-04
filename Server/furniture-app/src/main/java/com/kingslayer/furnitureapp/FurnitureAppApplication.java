a09d385af21c3b286b9a560ba94b910f83c0a82e
Himanshu Sherkar <sherkarhimanshu00@gmail.com>
2024-02-04 09:36:21 +0530

    Server setup

---
+++ Server/furniture-app/src/main/java/com/kingslayer/furnitureapp/FurnitureAppApplication.java
@@ -0,0 +1,12 @@
+package com.kingslayer.furnitureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureAppApplication.class, args);
+	}
+}
