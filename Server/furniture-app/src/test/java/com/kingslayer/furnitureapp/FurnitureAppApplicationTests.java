b78cd19a35d0e642c9918bec333cf80facbdc33b
Himanshu Sherkar <sherkarhimanshu00@gmail.com>
2024-02-04 09:36:21 +0530

    Server setup

---
+++ Server/furniture-app/src/test/java/com/kingslayer/furnitureapp/FurnitureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingslayer.furnitureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
