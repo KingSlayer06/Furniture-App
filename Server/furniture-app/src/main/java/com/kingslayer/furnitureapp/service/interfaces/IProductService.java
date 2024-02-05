55c5c35d86122d4eea66a535449da12b5d9c1789
Himanshu Sherkar <sherkarhimanshu00@gmail.com>
2024-02-05 22:38:54 +0530

    Fetch all products API implemented

---
+++ Server/furniture-app/src/main/java/com/kingslayer/furnitureapp/service/interfaces/IProductService.java
@@ -0,0 +1,9 @@
+package com.kingslayer.furnitureapp.service.interfaces;
+
+import com.kingslayer.furnitureapp.entity.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    public List<Product> getAllProducts();
+}
